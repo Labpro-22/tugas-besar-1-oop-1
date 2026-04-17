b65c554aabb399840738cf234046b279d540e7dc
Muhammad Akmal <muhammad.akmal.3806@gmail.com>
2026-04-17 10:49:09 +0700

    feat: add color and constants headers

---
+++ include/ui/Constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ui::dim {
+
+constexpr float multiplier = 80; /**< Choose 120 for FHD, otherwise HD */
+constexpr float width = 16 * multiplier;
+constexpr float height = 9 * multiplier;
+constexpr float sideWidth = 3.5f * multiplier;
+
+}  // namespace ui::dim
+
+namespace ui::font {}
