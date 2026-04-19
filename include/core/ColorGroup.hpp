9841fa3fb8af88b20a68d6244c827149d6981cec
aureliajenn <aurellia.jennifer@gmail.com>
2026-04-19 08:36:34 +0700

    feat: add ColorGroup header file

---
+++ include/core/ColorGroup.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace core {
+
+enum class ColorGroup {
+    COKLAT,
+    BIRU_MUDA,
+    MERAH_MUDA,
+    ORANGE,
+    MERAH,
+    KUNING,
+    HIJAU,
+    BIRU_TUA,
+    DEFAULT  // utk Railroad, Utility, petak aksi
+};
+
+}
